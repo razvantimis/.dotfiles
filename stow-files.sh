6bd5010bde6493f54d0e883a6c83ed521630812e
Razvan Timis <razvan.timis@8x8.com>
2023-06-15 11:49:26 +0300

    Add stow script

---
+++ stow-files.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# This script is used to stow files in the home directory.
+if [[ -z $STOW_FOLDERS ]]; then
+    STOW_FOLDERS="tmux"
+fi
+
+if [[ -z $DOTFILES ]]; then
+    DOTFILES=$HOME/.dotfiles
+fi
+echo "stow folders: $STOW_FOLDERS"
+echo "dotfiles: $DOTFILES"
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    echo "stow $folder"
+    stow -D $folder
+    stow $folder
+done
